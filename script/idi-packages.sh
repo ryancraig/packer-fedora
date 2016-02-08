9771f76dd85089bbeb5d1241139e1703b101860e
Giovanni Tirloni <gpt@gtirloni.com>
2016-02-08 09:39:48 -0200

    Remove version requirement for Firefox/Chrome with the goal of always tracking the latest version.
    It should be noted the Chrome repository does not keep older versions available once a new one is released, so this more of a requirement to make things work in case the box needs to be updated. For Firefox the situation isn't as such because older packages are still made available by Fedora.

--- script/idi-packages.sh
+++ script/idi-packages.sh
@@ -16,4 +16,4 @@ EOF
 ${PKG_MGR} install -y ansible curl bzip2 deltarpm gcc git kernel-devel kernel-headers make net-tools nfs-utils orca perl sqlite-devel sudo tar wget yum-utils zlib-devel
 
 # The remaining packages are GPII Framework dependencies - https://github.com/gpii/linux
-${PKG_MGR} install -y glib2-devel PackageKit-glib-devel json-glib-devel libXrandr-devel libXrender-devel libX11-devel xorg-x11-proto-devel alsa-lib-devel tuxguitar firefox-38.0.1 google-chrome-stable-46.0.2490.86
+${PKG_MGR} install -y glib2-devel PackageKit-glib-devel json-glib-devel libXrandr-devel libXrender-devel libX11-devel xorg-x11-proto-devel alsa-lib-devel tuxguitar firefox google-chrome-stable
