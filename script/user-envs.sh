bcfcad6fcc01e19e6764f926c77e4bdc04819838
Alfredo Matas <amatas@gmail.com>
2016-09-21 17:59:08 +0200

    Better description and change of directory name

--- script/user-envs.sh
+++ script/user-envs.sh
@@ -14,18 +14,18 @@ EOF
 cat <<-EOF > /usr/local/bin/save-env-vars.sh
 #!/bin/bash
 # This script saves the DBUS* and DISPLAY environment variables in a file
-# inside an user's directory in the /tmp. Then this variables will be loaded
+# located in the /tmp directory. These variables will be made available
 # in any terminal even if it's a SSH session
 
-mkdir -m 0700 /tmp/\$USER-DBUS-vars
-for i in \$(env | grep '^DBUS\|^DISPLAY'); do echo \$i >> /tmp/\$USER-DBUS-vars/vars; done
+mkdir -m 0700 /tmp/\$USER-DBUS-VARS
+for i in \$(env | grep '^DBUS\|^DISPLAY'); do echo \$i >> /tmp/\$USER-DBUS-VARS/vars; done
 
 EOF
 
 cat <<-EOF > /etc/profile.d/load-vars.sh
 # this command line loads the DBUS* and DISPLAY environment variables in any terminal
-if [ -f /tmp/\$USER-DBUS-vars/vars ]; then
-  for i in \$(cat /tmp/\$USER-DBUS-vars/vars | grep '^DISPLAY\|^DBUS');do export \$i; done
+if [ -f /tmp/\$USER-DBUS-VARS/vars ]; then
+  for i in \$(cat /tmp/\$USER-DBUS-VARS/vars | grep '^DISPLAY\|^DBUS');do export \$i; done
 fi
 
 EOF
