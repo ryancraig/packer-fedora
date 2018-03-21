25e3f5999520a7431d44d392cef0a3c8f7898e52
Giovanni Tirloni <gtirloni@ret.cx>
2018-03-21 14:47:57 -0300

    INFRA-147 - Switch from Docker 1.13 to Docker CE

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
-dnf install -y docker
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf install -y docker-ce
 systemctl enable docker
 groupadd docker
 usermod -a -G docker vagrant
